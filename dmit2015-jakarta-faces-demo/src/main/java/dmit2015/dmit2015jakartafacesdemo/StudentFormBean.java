6084961f198fe99940e5d81e7d68d62ba679eeb5
Tyler Hatch <Tyler_4155@hotmail.com>
2026-09-11 18:23:50 -0600

    Before Changes

---
+++ dmit2015-jakarta-faces-demo/src/main/java/dmit2015/dmit2015jakartafacesdemo/StudentFormBean.java
@@ -0,0 +1,13 @@
+package dmit2015.dmit2015jakartafacesdemo;
+
+
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+
+import java.io.Serializable;
+
+@Named
+@ViewScoped
+public class StudentFormBean implements Serializable {
+
+}
